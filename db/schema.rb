88eb9570254a5bf065a72e0476504f32062c4588
yanagi1111 <yo0hovbrink@gmail.com>
2024-10-19 11:57:33 +0900

    プルダウンの実装

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,19 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_10_19_022238) do
+  create_table "installs", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_installs_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true
+  end
+
   create_table "room_users", charset: "utf8mb3", force: :cascade do |t|
     t.bigint "room_id", null: false
     t.bigint "user_id", null: false
